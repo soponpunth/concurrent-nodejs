d11c6d45f36f8f26d4a3753f48b5085033586a5b
soponpunth <peach.ronakorn@gmail.com>
2015-07-24 11:21:18 +0200

    Update example.js

--- example.js
+++ example.js
@@ -3,19 +3,19 @@ var Workpool = require('./workpool.js');
 
 //init new workpool object with 2 concurrent functions
 var workpoolInstance = new Workpool(2, function(datalist){
-  console.log('alldone');
+  console.log('all done');
   //do whatever here with returned data
   // {"tag1": somedata1, "tag2": somedata2}
 });
 
 //perform asynchronous function call and store returned jsondata in Workpool class 		  
 someobject.functioncall(params, function(jsonData){
-  console.log('large');
+  console.log('big task');
   workpoolInstance.done('tag1',jsonData);
 });
 
 //perform another asynchronous function call and store returned jsondata in Workpool class 		  
 someobject.functioncall2(params, function(jsonData){
-  console.log('small');
+  console.log('small task');
   workpoolInstance.done('tag2',jsonData);
 });		
