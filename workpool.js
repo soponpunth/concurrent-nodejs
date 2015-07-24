085f44cf2298fe8839f82a40631a3406f4b0ba79
soponpunth <peach.ronakorn@gmail.com>
2015-07-24 11:17:41 +0200

    added

--- workpool.js
+++ workpool.js
@@ -1,12 +1,12 @@
 function Workpool(targetFunc, callback) {
-	this._dataPools = {};
+    this._dataPools = {};
     this._targetFunc = targetFunc;
     this._finishedFunc = 0;
     this._callback = callback;
 }
 
 Workpool.prototype.done = function(tag, data) {
-	this._dataPools[tag] = data;   
+    this._dataPools[tag] = data;   
     this._finishedFunc++;
 
     if(this._finishedFunc == this._targetFunc){
